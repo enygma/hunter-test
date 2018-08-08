4f1591e7d736ca2a60fae2a9096c233f6b034763
Chris Cornutt <ccornutt@ccornutt-16213.local>
2018-08-08 11:11:31 -0600

    adding all of the new logic for logins, registration and code input

--- app.py
+++ app.py
@@ -3,17 +3,19 @@ from flask import render_template
 from flask import request
 import bcrypt
 import json
+import random
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return render_template('index.html', msg="Hello 123!")
+def index():
+    return render_template('index.html')
 
 @app.route("/user/login", methods=['GET', 'POST'])
 def login():
     success = ''
     error = ''
+    code = ''
 
     if (request.method == 'POST'):
         username = request.form['username']
@@ -26,10 +28,14 @@ def login():
         if (user == None):
             error = 'User ' + username + ' not found'
 
-        bcryptCheck = bcrypt.checkpw(password.encode('utf8'), user['password'].encode('utf8')) == True
+        if bcrypt.checkpw(password.encode('utf8'), user['password'].encode('utf8')) == True:
+            code = random.randint(1000,9999)
 
-        if secureCompare(password, user['password']) or bcryptCheck == True:
-            success = 'Login successful for user ' + username
+            # Update the user record with the code
+            updateUser(username, None, code)
+
+            success = ('Login successful for user ' + username 
+                + '. To complete the login, use the code "%s" on the following page.') % code
         else:
             error = 'Invalid password'
 
@@ -38,19 +44,87 @@ def login():
     else:
         return render_template('login.html', success=success)
 
-@app.route("/user/register")
+@app.route("/user/register", methods=['GET', 'POST'])
 def register():
-    msg = ''
+    error = ''
+    success = ''
     
     if (request.method == 'POST'):
-        msg = 'submitted'
+        username = request.form['username']
+        password = request.form['password']
 
-    return render_template('register.html', msg=msg)
+        if (len(username) == 0):
+            error = 'Username is a required field'
+
+        if (len(password) == 0):
+            error = 'Password is a required field'
+
+        if (addUser(username, password) == True):
+            success = 'User registered successfully!'
+        else:
+            error = 'Error registering user!'
+
+    if (len(error) > 0):
+        return render_template('register.html', error=error)
+    else:
+        return render_template('register.html', success=success)
+
+@app.route('/user/code', methods=['GET', 'POST'])
+def code():
+    error = ''
+    success = ''
+
+    if (request.method == 'POST'):
+        username = request.form['username']
+        code = request.form['code']
+        user = findUser(username)
+
+        if not code:
+            code = None
+
+        if (secureCompare(code, user['code']) == True):
+            success = "Success! You're now logged in as " + username
+        else:
+            error = 'Error with the code provided'
+
+    if (len(error) > 0):
+        return render_template('code.html', error=error)
+    else:
+        return render_template('code.html', success=success)
 
 def readJson():
     with open('users.json') as json_file:
         data = json.load(json_file)
         return data
+
+def writeJson(data):
+    with open('users.json', 'w') as f:
+        json.dump(data, f)
+
+    return True
+
+def addUser(username, password):
+    data = readJson()
+    newUser = {
+        'username': username,
+        'password': bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt()),
+        'code': 'None'
+    }
+    data['users'].append(newUser)
+    return writeJson(data)
+
+def updateUser(username, password=None, code=None):
+    data = readJson()
+    for u in data['users']:
+        if u['username'] == username:
+            if (password != None):
+                u['password'] = password
+
+            if (code != None):
+                u['code'] = code
+    
+    writeJson(data)
+    return True
 
 def findUser(username):
     for u in readJson()['users']:
@@ -61,4 +135,4 @@ def secureCompare(input1, input2):
     bytes1 = bytearray(str(input1))
     bytes2 = bytearray(str(input2))
 
-    return len(bytes1) != len(bytes2)
+    return len(bytes1) == len(bytes2)
